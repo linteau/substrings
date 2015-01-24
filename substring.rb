dfddd89c2a15a3538f0c990032910b29ba1248c4
andylinteau <andy.linteau@gmail.com>
2015-01-24 01:57:50 -0500

    add multi text functionality to substring

--- substring.rb
+++ substring.rb
@@ -3,10 +3,18 @@ def substrings(string, dictionary)
 
 	words = []
 
-	dictionary.each do |word|
-		if string.include?(word)
-			words = words.push(word)
+	input_strings = string.split(" ")
+
+	input_strings.size.times do |i|
+
+		current_string = input_strings[i].downcase
+
+		dictionary.each do |word|
+			if current_string.include?(word)
+				words = words.push(word)
+			end
 		end
+
 	end
 
 	counts = Hash.new(0)
@@ -18,4 +26,4 @@ end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 
- substrings("below", dictionary)
+ substrings("Howdy partner, sit down! How's it going?", dictionary)
